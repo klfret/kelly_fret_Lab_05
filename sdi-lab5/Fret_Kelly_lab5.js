50a89e78a0761a689ea5e1e92b94a0f874529684
Kelly <KelCin@Kellys-MacBook-Pro.local>
2014-09-13 16:15:04 -0400

    rich test

--- sdi-lab5/Fret_Kelly_lab5.js
+++ sdi-lab5/Fret_Kelly_lab5.js
@@ -8,7 +8,7 @@ var operSignifier;
 var calculationResult;
 
 
-// division function
+// ******* division function *******
 var divisionFunction = function(div_one, div_two) {
 	
 	var divResult;
@@ -19,21 +19,45 @@ var divisionFunction = function(div_one, div_two) {
 	
 };
 
+
+// ******* multiplication function *******
+var multiplicationFunction = function(mul_one, mul_two) {
+	
+	var mulResult;
+	
+	mulResult = mul_one * mul_two;
+	
+	return mulResult;
+	
+};
+
+
 // obtain user's numbers and operation signifier
 firstNumber = parseFloat(prompt("Enter first number.", " 6 "));
 secondNumber = parseFloat(prompt("Enter second number.", " 17 "));
 operSignifier = prompt("Enter operation signifer.", " + ");
 
-// if division signifier
+// if user tries to divide by zero
+if (secondNumber == 0) {
+	secondNumber = parseFloat(prompt("You cannot divide by zero. \n\nEnter another number."));
+}
+
+
+// if operation signifier is division
 if (operSignifier == "/") {
 	
 	calculationResult = divisionFunction(firstNumber, secondNumber);
 	
-	console.log(firstNumber + operSignifier + secondNumber + " = " + calculationResult);
+	
+// if operation signifier is multiplication
+} else if (operSignifier == "*") {
+	
+	calculationResult = multiplicationFunction(firstNumber, secondNumber);
+	
 }
 
-
-
+console.log(firstNumber + " " + operSignifier + " " + secondNumber + " = " + calculationResult);
+//m
 
 
 
